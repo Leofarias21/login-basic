cac6504185bf02a51dd5b9258c61395c67cbcfe7
L. Farias <77168535+Leofarias21@users.noreply.github.com>
2022-10-27 16:04:46 -0300

    Delete python directory

--- python/Login.py
+++
@@ -1,8 +0,0 @@
-import getpass
-usuario= input("Digite o usuário: ").upper()
-senha=getpass.getpass("Digite a senha: ")
-
-if usuario =="BITMED" and senha=="Fiap1222":
-    print("Usuário logado")
-else: 
-    print("Usuário negado")
